a38ca065f06b87eeea1368d5f61e91c5cbf0d568
anirudh loona <anirudhloona26@gmail.com>
2022-08-08 00:43:17 +0530

    wrote first testing method

---
+++ src/test/java/com/testing/unitTesting/Calculator.java
@@ -0,0 +1,11 @@
+package com.testing.unitTesting;
+
+public class Calculator {
+	public int doSum(int a,int b,int c) {
+		return a+b+c;
+	}
+	
+	public int doMultipy(int a,int b) {
+		return a*b;
+	}
+}
--- src/test/java/com/testing/unitTesting/UnitTestingApplicationTests.java
+++ src/test/java/com/testing/unitTesting/UnitTestingApplicationTests.java
@@ -2,12 +2,26 @@ package com.testing.unitTesting;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class UnitTestingApplicationTests {
 
+	private Calculator calculator = new Calculator();
+	
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testSum() {
+		int expect = 33;
+		
+		//actual;
+		int result = calculator.doSum(12, 11, 10);
+		
+//		we have to check if actual result matches with expected result 
+//		to do so we will use a library called as AssertJ
+		assertThat(result).isEqualTo(expect);
+	}
 }
