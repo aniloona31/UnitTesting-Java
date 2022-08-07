c4498dc82e690cac2cc2713a68f0048c331ed91c
anirudh loona <anirudhloona26@gmail.com>
2022-08-07 23:44:56 +0530

    first commit

---
+++ src/main/java/com/testing/unitTesting/Controllers/PersonController.java
@@ -0,0 +1,5 @@
+package com.testing.unitTesting.Controllers;
+
+public class PersonController {
+
+}
---
+++ src/test/java/com/testing/unitTesting/UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.unitTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
